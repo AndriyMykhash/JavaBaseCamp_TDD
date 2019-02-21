411cf1bbdbce9c471b014342d9ec19121e10f7d8
Андрій Михаш <capitaw8@gmail.com>
2019-02-21 21:53:11 +0200

    Add test case

--- src/main/java/com/company/Caesar.java
+++ src/main/java/com/company/Caesar.java
@@ -2,8 +2,23 @@ package com.company;
 
 public class Caesar {
 
+    char[] letter = new char[]{
+            'a','b','c', 'd', 'e', 'f', 'g', 'h', 'i',
+            'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',
+            'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+    };
 
-    public String caesarCipher(String toCodeMessage, int steps) {
+    public String caesarCipher(String toEncodeMessage, int steps) {
+        StringBuilder codedMessage = new StringBuilder();
+
+
+
+
         return null;
+    }
+
+
+    public char encodeChar(char toEncode, int key) {
+        return '\n';
     }
 }
--- src/test/java/com/test/TestClass.java
+++ src/test/java/com/test/TestClass.java
@@ -19,4 +19,11 @@ public class TestClass {
         Assert.assertEquals("axvjrwn unccdln", unit.caesarCipher("romaine lettuce",9) );
     }
 
+    @Test
+    public void getCipherChar(){
+        Assert.assertEquals('i',unit.encodeChar('a',9));
+        Assert.assertEquals('a',unit.encodeChar('r',9));
+
+    }
+
 }
